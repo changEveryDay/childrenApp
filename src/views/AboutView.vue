ba7f3328cebfdb9a666eaedf2ef755f76f7a0b59
Yongfang QU 屈永芳 <yongfang.qu@nio.com>
2023-05-17 21:43:16 +0800

    inite

---
+++ src/views/AboutView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <el-button type="primary" style="margin-left: 16px" @click="drawer = true">
+    点击打开drawer
+  </el-button>
+
+  <el-drawer v-model="drawer">
+    <template #header>
+      <h4>set title by slot</h4>
+    </template>
+    <template #default>
+      <div>
+        <el-radio v-model="radio1" label="Option 1" size="large"
+          >Option 1</el-radio
+        >
+        <el-radio v-model="radio1" label="Option 2" size="large"
+          >Option 2</el-radio
+        >
+      </div>
+    </template>
+    <template #footer>
+      <div style="flex: auto">
+        <el-button @click="drawer = false">cancel</el-button>
+        <el-button type="primary" >confirm</el-button>
+      </div>
+    </template>
+  </el-drawer>
+  </div>
+</template>
+<script>
+export default{
+    name: "ResultRecords",
+    data() {
+        return {
+           drawer: false
+        }
+    },
+  }
+</script>
